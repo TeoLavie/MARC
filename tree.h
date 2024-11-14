de6c2a9d2372c3283b475849e47e54803ecd4ba8
StonksProp <151038048+StonksProp@users.noreply.github.com>
2024-11-14 08:41:22 +0100

    t_node pas modif

--- tree.h
+++ tree.h
@@ -6,3 +6,25 @@
 #define UNTITLED1_TREE_H
 
 #endif //UNTITLED1_TREE_H
+
+
+typedef struct s_node{
+    int value;
+    int depth;
+    struct s_node **sons;
+    int nbSons
+}t_node;
+
+t_node *createNode(int val, int nb_sons)
+{
+    t_node *new node;
+    new_node = (t_node *)malloc(sizeof(t_node));
+    new_node->value = val;
+    new_node->nbSons = nd_sons;
+    new_node->sons = (t_node **)malloc(nb_sons*sizeof(t_node *));
+    for (int i=0; i <nb_sons; i++)
+    {
+        new_node->sons[i]=NULL;
+    }
+    return new_node;
+}
