c3af6c6fb478f99d1f368ac45429e45fec05aa54
Stonksnotprop <max.dambron@efrei.net>
2024-11-14 09:53:01 +0100

    tree type

--- tree.c
+++ tree.c
@@ -3,12 +3,14 @@
 //
 
 #include "tree.h"
+#include <stdlib.h>
+
 t_node *createNode(int val, int nb_sons, int depth)
 {
-    t_node *new node;
+    t_node *new_node;
     new_node = (t_node *)malloc(sizeof(t_node));
     new_node->value = val;
-    new_node->nbSons = nd_sons;
+    new_node->nbSons = nb_sons;
     new_node->depth = depth;
     new_node->sons = (t_node **)malloc(nb_sons*sizeof(t_node *));
     for (int i=0; i <nb_sons; i++)
