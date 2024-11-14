ddf035e49b992c3012c1845f8d054e1b1bf6b0a4
StonksProp <151038048+StonksProp@users.noreply.github.com>
2024-11-14 08:40:32 +0100

    t_node pas modif

--- tree.c
+++ tree.c
@@ -9,3 +9,17 @@ typedef struct s_node{
     struct s_node **sons;
     int nbSons
 }t_node;
+
+t_node *createNode(int val, int nb_sons)
+{
+    t_node *new node;
+    new_node = (t_node *)malloc(sizeof(t_node));
+    new_node->value = val;
+    new_node->nbSons = nd_sons;
+    new_node->sons = (t_node **)malloc(nb_sons*sizeof(t_node *));
+    for (int i=0; i <nb_sons; i++)
+    {
+        new_node->sons[i]=NULL;
+    }
+    return new_node;
+}
